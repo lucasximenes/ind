2675435e5c6b7348661a1042cfdafd1b7f83a4ff
lucasximenes <lucasxg@aluno.puc-rio.br>
2022-11-17 19:54:36 -0300

    almost there

--- projeto final/robustcvar.jl
+++ projeto final/robustcvar.jl
@@ -13,32 +13,70 @@ function cvar(x, returns, α)
 end
 
 function backtest(window_size::Int64, brokerage_rate::Float64, risk_threshold::Float64,
-    start_date::Int64, n_steps::Int64, initial_funds)
+    start_date::Int64, n_steps::Int64, initial_funds, Γ::Vector{Float64}, benchmark::Bool)
+    
     if start_date > 1000
         println("Choose a starting date before 1000 days ago")
     end
+    
     #Reading returns data
     returns_df = CSV.read(joinpath(@__DIR__, "./returns.csv"), DataFrame)
+    
     #Converting to matrix
     returns = Matrix{Float64}(returns_df)
     returns = returns[(start_date - window_size - n_steps):start_date, :]
 
-    allocation = robust_cvar_allocation(returns[n_steps:(window_size + n_steps), :],
-        risk_threshold, 0.95, 1, initial_funds, 1, 1)
-    
-    portfolio_returns_future = zeros(n_steps)
-    #Start rolling window backtest
-    for i in 1:n_steps-1
-        portfolio_returns_future[i] = allocation ⋅ (1 .+ returns[n_steps - i, :])
-        allocation = robust_cvar_allocation(returns[(n_steps - i):(window_size + n_steps - i), :],
-         risk_threshold, 0.95, 1, portfolio_returns_future[i], 1, 1)
-        println(allocation)
-         if allocation === nothing
-            println("No allocation found for step ", i)
-            return portfolio_returns_future
-        end
+    #Naming dataframe columns
+    cols = ["Γ = $robustness" for robustness in Γ]
+
+    if benchmark
+        push!(cols, "Benchmark")
     end
-    return portfolio_returns_future
+
+    results_matrix = zeros(n_steps, length(cols))
+
+    uniform_allocation = [initial_funds/length(returns[1, :]) for i in 1:length(returns[1, :])]
+    
+    for (index, robustness) in enumerate(Γ)
+    
+        window = returns[n_steps:(window_size + n_steps), :]
+
+        allocation = robust_cvar_allocation(window, risk_threshold, 0.95, robustness, initial_funds, brokerage_rate, uniform_allocation)
+        
+        portfolio_returns_future = zeros(n_steps)
+        
+        println(allocation, size(window))
+
+        #Start rolling window backtest
+        for i in 1:n_steps-1
+            portfolio_returns_future[i] = allocation ⋅ (1 .+ returns[n_steps - i, :])
+            allocation = robust_cvar_allocation(returns[(n_steps - i):(window_size + n_steps - i), :],
+            risk_threshold, 0.95, robustness, portfolio_returns_future[i], brokerage_rate, allocation)
+            if allocation === nothing
+                println("No allocation found for step ", i)
+                break
+            end
+        end
+
+        results_matrix[:, index] = portfolio_returns_future
+    
+    end
+
+    if benchmark
+        
+        allocation = copy(uniform_allocation)
+        portfolio_returns_future = zeros(n_steps)
+
+        for i in 1:n_steps-1
+            portfolio_returns_future[i] = allocation ⋅ (1 .+ returns[n_steps - i, :])
+            allocation = allocation .* (1 .+ returns[n_steps - i, :])
+        end
+        results_matrix[:, end] = portfolio_returns_future
+    
+    end
+
+    return DataFrame(results_matrix, cols)
+
 end
 
 function robust_cvar_allocation(returns, T_r, α, Γ, funds, γ, former_allocation)
@@ -52,22 +90,25 @@ function robust_cvar_allocation(returns, T_r, α, Γ, funds, γ, former_allocati
     σ = σ[1,:]
     @variables(m,
     begin
-        P
+        P ≥ 0
         x[1:n_assets] ≥ 0
-        λ[1:n_assets] ≥ 0
+        λ[1:n_assets] ≤ 0
         Δ_p[1:n_assets] ≥ 0
-        Δ_n[1:n_assets] ≤ 0
+        Δ_n[1:n_assets] ≥ 0
         θ[1:n_scenarios] ≥ 0
         l
-        β
+        β ≤ 0
+        c[1:n_assets] ≥ 0
+        v[1:n_assets] ≥ 0
     end)
-    @constraint(m, con1, sum(x) == funds)
+    @constraint(m, con0, sum(x) == funds - γ*sum((c[n] + v[n]) for n in 1:n_assets))
+    @constraint(m, con1[n=1:n_assets], x[n] == former_allocation[n] + c[n] - v[n] - γ*(c[n] + v[n]))
     @constraint(m, con2, P ≤ r̄ ⋅ x)
     @constraint(m, con3[s=1:n_scenarios], θ[s] ≥ -returns[s, :] ⋅ x - l)
     @constraint(m, con4, l + sum(θ)*(1/n_scenarios)/(1-α) ≤ T_r*funds)
     @constraint(m, con5[n=1:n_assets], -Δ_p[n] + Δ_n[n] ≥ -x[n])
     @constraint(m, con6[n=1:n_assets], -σ[n]*(Δ_p[n] + Δ_n[n]) - β - λ[n] ≥ 0)
-    @constraint(m, con7[n=1:n_assets], -Γ*β - sum(λ) + sum(r̄[n]*(Δ_p[n] - Δ_n[n])) ≥ P)
+    @constraint(m, con7, Γ*β + sum(λ) + sum(r̄ .* (Δ_p .- Δ_n)) ≥ P)
     @objective(m, Max, P)
     optimize!(m)
     #Check if the problem is feasible
@@ -82,9 +123,11 @@ end
 #Reading returns data
 returns_df = CSV.read(joinpath(@__DIR__, "./returns.csv"), DataFrame)
 mat = Matrix{Float64}(returns_df)
-allocation = robust_cvar_allocation(mat, 0.039, 0.95, 1, 10e6, 0.01, 0.01)
+initial_allocation = [10e6/length(mat[1, :]) for i in 1:length(mat[1, :])]
+
+allocation = robust_cvar_allocation(mat, 0.05, 0.95, 2, 10e6, 0.01, initial_allocation)
 
 cvar(allocation, mat, 0.95)
 
 
-returns_vec = backtest(100, 0.01, 0.1, 300, 50, 10e6)
+final_df = backtest(100, 0.01, 0.05, 300, 50, 10e6, [0.1, 0.25, 0.5, 0.75, 1], true)
